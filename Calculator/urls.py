100eeb1200e92b9a0b5fda3a8ae7d6c91fc12b1c
Azizsy <aasy.ndiaye@univ-thies.sn>
2022-06-22 02:14:49 +0000

    Update urls.py

--- Calculator/urls.py
+++ Calculator/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('add',views.addition, name='add'),
-    path('sub',views.subtraction, name='sub'),
+   
     path('multi',views.multiplication, name='multi'),
-    path('div',views.division, name='div')
+
 ]
