a0e3e4f5d30edc51419522d150f8ac1c34e1e70c
Sada Demba Thiam <sadadembat9@gmail.com>
2022-06-22 07:06:13 +0000

    deploiement

--- WebCalculator/settings.py
+++ WebCalculator/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-@ekc@h_p5qy0s4-u6&k-bd&p4u!k4q*31bs28cmyplb8k#w_c2'
-SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
+SECRET_KEY = os.getenv('KEY', 'Optional default value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
