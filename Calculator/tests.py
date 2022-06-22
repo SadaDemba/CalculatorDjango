09b55896211f20d52f522d03480a71a9c95412a2
Massaer-Diop <diopmassaer555@gmail.com>
2022-06-22 04:07:39 +0000

    Implementation de la vue du model et du test (Addition et Division)

--- Calculator/tests.py
+++ Calculator/tests.py
@@ -4,6 +4,16 @@ from Calculator.models import Operation
 
 class TestOperations(TestCase):
   
+    def testAddition(self):
+        addition1=Operation('10','5')
+        addition1.addition()
+        self.assertEqual(addition1.getResult(),15,"Succès du test!")
+
+        addition2=Operation('10a','4')
+        addition2.addition()
+        self.assertEqual(addition2.getResult(),"Caractère non permis","Succès du test!")
+
+    
     def testMultiplication(self):
         multiplication1=Operation('10','5')
         multiplication1.multiplication()
@@ -12,5 +22,19 @@ class TestOperations(TestCase):
         multiplication2=Operation('10a','4')
         multiplication2.multiplication()
         self.assertEqual(multiplication2.getResult(),"Caractère non permis","Succès du test!")
+
+    def testdivision(self):
+        division1=Operation('10','5')
+        division1.division()
+        self.assertEqual(division1.getResult(),2,"Succès du test!")
+
+        division2=Operation('10','0')
+        division2.division()
+        self.assertEqual(division2.getResult(),"Impossible de diviser par 0","Succès du test!")
+
+        division3=Operation('10a','4')
+        division3.division()
+        self.assertEqual(division3.getResult(),"Caractère non permis","Succès du test!")
+
 
      
--- Calculator/views.py
+++ Calculator/views.py
@@ -4,6 +4,15 @@ from Calculator.models import Operation
 def index(request):
     return render(request, "input.html")
 
+def addition(request):
+
+    op1:str = request.POST['num1']
+    op2:str = request.POST['num2']
+
+    op=Operation(op1,op2)
+    op.addition()
+    return render(request, "result.html", {"result": op.getResult()})
+
 
 def multiplication(request):
 
@@ -14,3 +23,10 @@ def multiplication(request):
     return render(request, "result.html", {"result": op.getResult()})
 
 
+def division(request):
+
+    num1 = request.POST['num1']
+    num2 = request.POST['num2']
+    op=Operation(num1,num2)
+    op.division()
+    return render(request, "result.html", {"result": op.getResult()})
