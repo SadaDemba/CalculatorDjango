9ed6407ad44dba724bc8e1a5d8a29276c2ec1944
Sada Demba Thiam <sadadembat9@gmail.com>
2022-06-22 04:36:06 +0000

    Remove a hardcoded Django secret key

--- WebCalculator/settings.py
+++ WebCalculator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ekc@h_p5qy0s4-u6&k-bd&p4u!k4q*31bs28cmyplb8k#w_c2'
+#SECRET_KEY = 'django-insecure-@ekc@h_p5qy0s4-u6&k-bd&p4u!k4q*31bs28cmyplb8k#w_c2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
